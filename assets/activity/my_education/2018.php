bc02e1c5d2fdbba565c38987230295767f77d4f0
JamesSerenio <sereniojames363@gmail.com>
2024-10-06 23:52:18 +0800

    adding 2018.php and css

---
+++ assets/activity/my_education/2018.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Year 2018</title>
+    <link rel="stylesheet" type="text/css" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../activity/my_education_css/2018.css">
+</head>
+<body>
+
+<a href="javascript:history.back()" class="back-button">
+        <img src="../../image/back.gif" alt="Back">
+    </a>
+
+    <section class="year-section">
+        <h1>Year 2018</h1>
+        <h3>Sports</h3>
+        <p>"This year, I joined the swimming team for our school, and we practiced for a year for the upcoming competition."</p>
+        <img src="../../image/sport1.jpg" alt="Project Image">
+    </section>
+
+    <script src="../../js/bootstrap.bundle.min.js"></script>
+    <script src="../../js/jquery-3.7.1.js"></script>
+</body>
+</html>
